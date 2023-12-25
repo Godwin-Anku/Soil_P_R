adffe0224d6cedcc6089d48032cd36700a0ba717
Godwin-Anku <godwinanku246@gmail.com>
2023-12-25 04:57:07 -0800

    Update on computation of parameters

--- soil_phase_relationship.cpp
+++ soil_phase_relationship.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(){
-    float s, Vw, sat, vol_of_voids, vol_of_solids, vol_of_soil,
+    float s, Vw, sat, volofVoids, vol_of_voids, Vv, vol_of_solids, vol_of_soil,
     porosity, WaterContent, weight_of_sample, weight_of_dry_sample,
     weight_of_wet_sample, mass_of_sample, density, mass, volume,
     SatUnitWeight, unit_weight_of_water, SGravity_of_solid, VoidRatio,
@@ -30,11 +30,34 @@ int main(){
             cout << "1. Degree of Saturation" << endl;
             cout << "Enter volume of water: ";
             cin >> Vw;
-            cout << "Enter volume of voids: ";
+            cout << "Enter value for volume of voids: ";
             cin >> vol_of_voids;
             s = Vw / vol_of_voids;
             s *= 100;
             cout << "\nPercentage of degree of saturation = " << s << endl;
+            break;
+        }
+        case 2:{
+            cout << "Lunar Systems" << endl;
+            cout << "2. Void Ratio" << endl;
+            cin >> Vv;
+            cout << "Enter volume of voids: " << endl;
+            cout << "Enter value for volume of solids: " << endl;
+            cin >> vol_of_solids;
+            voidRatio = Vv / vol_of_solids;
+            cout << "Void Ratio: " << endl;
+            break;
+        }
+        case 3:{
+            cout << "Lunar Systems" << endl;
+            cout << "Porosity" << endl;
+            cout << "Enter value for volume of voids" << endl;
+            cin >> volofVoids;
+            cout << "Enter value for volume of soil: " << endl;
+            cin >> vol_of_soil;
+            porosity = volofVoids / vol_of_soil;
+            porosity *= 100;
+            cout << "Percentage of Porosity: " << porosity;
             break;
         }
     }
